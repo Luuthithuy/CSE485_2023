208439f2fa07011c0050c989b471f87d48d9f730
Luuthithuy <luuthithuy@gmail.com>
2023-05-22 01:31:31 +0700

    update BTTH02

---
+++ BTTH02/ClassTeacher/Includes/sidebar.php
@@ -0,0 +1,53 @@
+ <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
+      <a class="sidebar-brand d-flex align-items-center bg-gradient-primary justify-content-center" href="index.php">
+        <div class="sidebar-brand-icon" >
+          <img src="img/logo/Logo-Thuy_Loi.png">
+        </div>
+      </a>
+      <hr class="sidebar-divider my-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Trang chủ</span></a>
+      </li> 
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+        Sinh viên
+      </div>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseBootstrap2"
+          aria-expanded="true" aria-controls="collapseBootstrap2">
+          <i class="fas fa-user-graduate"></i>
+          <span>Quản lý sinh viên</span>
+        </a>
+        <div id="collapseBootstrap2" class="collapse" aria-labelledby="headingBootstrap" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <a class="collapse-item" href="viewStudents.php">Danh sách sinh viên</a>
+          </div>
+        </div>
+      </li>
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+      Điểm danh
+      </div>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseBootstrapcon"
+          aria-expanded="true" aria-controls="collapseBootstrapcon">
+          <i class="fa fa-calendar-alt"></i>
+          <span>Quản lý điểm danh</span>
+        </a>
+        <div id="collapseBootstrapcon" class="collapse" aria-labelledby="headingBootstrap" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <a class="collapse-item" href="takeAttendance.php">Tạo điểm danh</a>
+            <a class="collapse-item" href="viewAttendance.php">Lịch sử điểm danh</a>
+            <a class="collapse-item" href="viewStudentAttendance.php">Thống kê sinh viên</a>
+            <a class="collapse-item" href="downloadRecord.php">Danh sách điểm danh (xls)</a>
+          </div>
+        </div>
+      </li>
+
+      <hr class="sidebar-divider">
+     
+    </ul>
