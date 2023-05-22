a677b45f45c2238b412c2e6316746ba2783d3156
Ngô Thảo Linh <thaolinhngo02@gmail.com>
2023-05-22 07:14:16 +0700

    Database

---
+++ BTTH02/classes.sql
@@ -0,0 +1,4 @@
+INSERT INTO classes (class_ID,course_id,instructor_id,class_time)
+VALUES ('62TH2',485,1,'Thứ 3 từ 12:55 đến 14:40'),
+('62TH3',263,2,'Thứ 2 từ 7:00 đến 9:40'),
+('62TH4',345,3,'Thứ 5 từ 10:40 đến 12:25');
